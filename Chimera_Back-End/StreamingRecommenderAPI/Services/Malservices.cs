ef82f5f433e02058411dccb26521a8223902ad56
Luiz-sun <luizhenriqueribeiroleite6@gmail.com>
2025-08-28 22:17:44 -0300

    serviço dos catalogos

---
+++ Chimera_Back-End/StreamingRecommenderAPI/Services/Malservices.cs
@@ -0,0 +1,35 @@
+﻿
+using Newtonsoft.Json;
+using Apianime.models.Catalogo;
+using Apianime.models.Mal;
+namespace Apianime.Services;
+
+public class Malservices
+{
+    private readonly HttpClient _httpClient;
+    private const string ClientId = "SEU_CLIENT_ID_MAL";
+
+    public Malservices(HttpClient client)
+    {
+        _httpClient = client;
+        _httpClient.BaseAddress = new Uri("https://api.myanimelist.net/v2/");
+        _httpClient.DefaultRequestHeaders.Add("X-MAL-CLIENT-ID", ClientId);
+    }
+
+    public async Task<List<CatalogoAnime>> GetTopAnimes()
+    {
+        var json = await _httpClient.GetStringAsync("anime/ranking?ranking_type=all&limit=10");
+        var animes = JsonConvert.DeserializeObject<AnimeListResponse>(json);
+
+        return animes.data.Select(a => new CatalogoAnime
+        {
+            Id = a.node.id,
+            Titulo = a.node.title,
+            Tipo = "Anime",
+            Imagem = a.node.main_picture?.large,
+            Nota = a.node.mean,
+            Episodios = null,
+            Origem = "MAL"
+        }).ToList();
+    }
+}
---
+++ Chimera_Back-End/StreamingRecommenderAPI/Services/Movelservices.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Apianime.models.Catalogo;
+using Apianime.models.Movel;
+
+namespace Apianime.Services;
+
+public class Movelservices
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey = "SUA_API_KEY_TMDB";
+
+    public Movelservices(HttpClient client)
+    {
+        _httpClient = client;
+        _httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/");
+    }
+
+    public async Task<List<CatalogoMovel>> GetPopularMovies()
+    {
+        var json = await _httpClient.GetStringAsync($"movie/popular?api_key={_apiKey}&language=pt-BR");
+        var filmes = JsonConvert.DeserializeObject<MovieListResponse>(json);
+
+        return filmes.results.Select(f => new CatalogoMovel
+        {
+            Id = f.id,
+            Titulo = f.title,
+            Tipo = "Filme",
+            Sinopse = f.overview,
+            Imagem = $"https://image.tmdb.org/t/p/w500{f.poster_path}",
+            Nota = f.vote_average,
+            DataLancamento = f.release_date,
+            DuracaoMinutos = null,
+            Origem = "TMDb"
+        }).ToList();
+    }
+}
