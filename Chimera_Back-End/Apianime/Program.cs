5ac6f9053b41c66da908b66d3cbf83a966892227
Luiz-sun <luizhenriqueribeiroleite6@gmail.com>
2025-09-01 20:50:22 -0300

    Mundas e alterando as pastas

---
+++ Chimera_Back-End/Apianime/Program.cs
@@ -0,0 +1,20 @@
+[ApiController]
+[Route("api/catalogo")]
+public class CatalogoController : ControllerBase
+{
+    private readonly CatalogoServices _catalog;
+
+    public CatalogoController(CatalogoServices catalog)
+    {
+        _catalog = catalog;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<CatalogoItem>>> GetCatalogo(
+        [FromQuery] string filtro = null,
+        [FromQuery] double? notaMin = null)
+    {
+        var lista = await _catalog.GetCatalog(filtro, notaMin);
+        return Ok(lista);
+    }
+}
--- Chimera_Back-End/StreamingRecommenderAPI/Program.cs
+++ Chimera_Back-End/StreamingRecommenderAPI/Program.cs
@@ -52,32 +52,3 @@ app.MapControllers();
 
 app.Run();
 
-//api Anime, serie e filmes
-
-using Apianime.models.Catalogo;
-using Apianime.Services;
-using Microsoft.AspNetCore.Mvc;
-
-
-namespace Apianime.Controller;
-
-[ApiController]
-[Route("api/catalogo")]
-public class CatalogoController : ControllerBase
-{
-    private readonly CatalogoServices _catalog;
-
-    public CatalogoController(CatalogoServices catalog)
-    {
-        _catalog = catalog;
-    }
-
-    [HttpGet]
-    public async Task<ActionResult<List<CatalogoItem>>> GetCatalogo(
-        [FromQuery] string filtro = null,
-        [FromQuery] double? notaMin = null)
-    {
-        var lista = await _catalog.GetCatalog(filtro, notaMin);
-        return Ok(lista);
-    }
-}
