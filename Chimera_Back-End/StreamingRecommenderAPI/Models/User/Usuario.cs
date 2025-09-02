7613e4e8399df430b8bb4b14e251b99245afa2b8
Arthur260907 <12301523@aluno.cotemig.com.br>
2025-09-02 00:48:55 +0000

    Update class usuario

--- Chimera_Back-End/StreamingRecommenderAPI/Models/User/Usuario.cs
+++ Chimera_Back-End/StreamingRecommenderAPI/Models/User/Usuario.cs
@@ -6,9 +6,9 @@ namespace Usuario.Models
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-        
-        public string TokenRecuperacao { get; set; }
-        public DateTime DataCriacao { get; set; }
-        public DateTime? DataUltimoAcesso { get; set; }
+        public DateTime Data_Cadastro { get; set; }
+        public string Foto_Perfil { get; set; }
+        public string? TokenRecuperacao { get; set; }
+        public DateTime? TokenExpiraEm { get; set; }
     }
 }
