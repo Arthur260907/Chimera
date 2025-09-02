9162311e8f9be347d87b3de7e67ca4e2cb25892a
Luiz-sun <luizhenriqueribeiroleite6@gmail.com>
2025-09-01 23:02:47 -0300

    Concertando um neocio do program.cs

--- Chimera_Back-End/Apianime/Program.cs
+++ Chimera_Back-End/Apianime/Program.cs
@@ -1,4 +1,29 @@
 <Project Sdk="Microsoft.NET.Sdk.Web">
+var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddControllers();
+// ...outras configurações...
+
+var app = builder.Build();
+
+// Usa CORS
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
 [ApiController]
 [Route("api/catalogo")]
 public class CatalogoController : ControllerBase
