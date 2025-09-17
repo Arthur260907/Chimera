24525b049b50e1fa7c12f34252e003513ccd6100
Luiz-sun <luizhenriqueribeiroleite6@gmail.com>
2025-09-17 10:31:13 -0300

    Add files via upload

---
+++ Chimera_Back-End/StreamingRecommenderAPI/StreamingRecommenderAPI/StreamingRecommenderAPI/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StreamingRecommenderAPI.Models.User;
+using System.Threading.Tasks;
+
+namespace StreamingRecommenderAPI.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario> GetByEmailAsync(string email);
+        Task<Usuario> GetByTokenAsync(string token);
+        Task SalvarTokenAsync(string email, string token, DateTime expiraEm);
+        Task AtualizarSenhaAsync(string token, string novaSenhaHash);
+        Task CadastrarUsuarioAsync(Usuario usuario);
+    }
+}
