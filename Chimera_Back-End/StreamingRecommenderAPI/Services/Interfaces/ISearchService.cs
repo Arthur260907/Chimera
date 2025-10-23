8b7bac010fc2579f3783a7cd261ed69c653c5719
Arthur260907 <12301523@aluno.cotemig.com.br>
2025-10-23 19:04:20 -0300

    Update ISearchService.cs

--- Chimera_Back-End/StreamingRecommenderAPI/Services/Interfaces/ISearchService.cs
+++ Chimera_Back-End/StreamingRecommenderAPI/Services/Interfaces/ISearchService.cs
@@ -1,6 +1,13 @@
-﻿namespace StreamingRecommenderAPI.Services.Interfaces
+// Services/Interfaces/ISearchService.cs
+using StreamingRecommenderAPI.Models; // Ajuste se OmdbMovie estiver em Models.Midia
+using StreamingRecommenderAPI.Models.Midia;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StreamingRecommenderAPI.Services.Interfaces
 {
-    public class ISearchService
+    public interface ISearchService
     {
+        Task<IEnumerable<OmdbMovie>> SearchAsync(string query);
     }
 }
