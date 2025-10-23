8d19da477a450f59666c0e425a133a52637357d2
Arthur260907 <12301523@aluno.cotemig.com.br>
2025-10-23 19:04:01 -0300

    Update IFilterService.cs

--- Chimera_Back-End/StreamingRecommenderAPI/Services/Interfaces/IFilterService.cs
+++ Chimera_Back-End/StreamingRecommenderAPI/Services/Interfaces/IFilterService.cs
@@ -1,6 +1,13 @@
-﻿namespace StreamingRecommenderAPI.Services.Interfaces
+// Services/Interfaces/IFilterService.cs
+using StreamingRecommenderAPI.Models; // Ajuste se OmdbMovie estiver em Models.Midia
+using StreamingRecommenderAPI.Models.Midia;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StreamingRecommenderAPI.Services.Interfaces
 {
-    public class IFilterService
+    public interface IFilterService
     {
+        Task<IEnumerable<OmdbMovie>> ExecuteAsync(string query);
     }
 }
