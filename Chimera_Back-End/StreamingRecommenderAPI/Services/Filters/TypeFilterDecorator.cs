24e4108713a4a7a3691d1450fdc3dc51503b332d
Arthur260907 <12301523@aluno.cotemig.com.br>
2025-10-23 19:02:36 -0300

    Update TypeFilterDecorator.cs

--- Chimera_Back-End/StreamingRecommenderAPI/Services/Filters/TypeFilterDecorator.cs
+++ Chimera_Back-End/StreamingRecommenderAPI/Services/Filters/TypeFilterDecorator.cs
@@ -1,6 +1,29 @@
-﻿namespace StreamingRecommenderAPI.Services.Filters
+// Services/Filters/TypeFilterDecorator.cs
+using StreamingRecommenderAPI.Models; // Namespace de OmdbMovie
+using StreamingRecommenderAPI.Services.Interfaces;
+using System; // Para StringComparison
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamingRecommenderAPI.Services.Filters
 {
-    public class TypeFilterDecorator
+    public class TypeFilterDecorator : FilterDecorator
     {
+        private readonly string _mediaTypeToFilter; // Ex: "movie", "series"
+
+        public TypeFilterDecorator(IFilterService innerFilter, string mediaTypeToFilter)
+            : base(innerFilter)
+        {
+            _mediaTypeToFilter = mediaTypeToFilter;
+        }
+
+        public override async Task<IEnumerable<Models.Midia.OmdbMovie>> ExecuteAsync(string query)
+        {
+            var results = await _innerFilter.ExecuteAsync(query);
+            return results.Where(item =>
+                !string.IsNullOrEmpty(item.Type) && // Garante que Type não é nulo/vazio
+                item.Type.Equals(_mediaTypeToFilter, StringComparison.OrdinalIgnoreCase));
+        }
     }
 }
