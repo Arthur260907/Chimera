3564236b973688e8542de140e5a19f825c160c92
IIKomneII <amanciootavio6@gmail.com>
2025-10-23 21:41:55 -0300

    fix(interfaces): resolve merge markers in ISearchService and IFilterService

--- Chimera_Back-End/StreamingRecommenderAPI/Services/Interfaces/IFilterService.cs
+++ Chimera_Back-End/StreamingRecommenderAPI/Services/Interfaces/IFilterService.cs
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using StreamingRecommenderAPI.Models.Midia;
-=======
-// Services/Interfaces/IFilterService.cs
-using StreamingRecommenderAPI.Models; // Ajuste se OmdbMovie estiver em Models.Midia
-using StreamingRecommenderAPI.Models.Midia;
 using System.Collections.Generic;
 using System.Threading.Tasks;
->>>>>>> 449ee9dbd286b960d244b48ad90eeb5a3c674493
+using StreamingRecommenderAPI.Models.Midia;
 
 namespace StreamingRecommenderAPI.Services.Interfaces
 {
+    /// <summary>
+    /// Interface que representa um serviço de filtros encadeáveis que recebe
+    /// uma query e retorna os filmes resultantes.
+    /// </summary>
     public interface IFilterService
     {
-<<<<<<< HEAD
-        // método assíncrono que aplica o filtro e retorna coleção filtrada
-        Task<IEnumerable<OmdbMovie>> ExecuteAsync(IEnumerable<OmdbMovie> source);
-=======
         Task<IEnumerable<OmdbMovie>> ExecuteAsync(string query);
->>>>>>> 449ee9dbd286b960d244b48ad90eeb5a3c674493
     }
 }
